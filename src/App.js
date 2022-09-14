d123ac541a7cc7734c5188dc354ba73c88bb91c5
Sararchh <sara_rocha99@hotmail.com>
2022-09-13 21:23:38 -0300

    feat: Refatorando codigo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Corpo } from './components/Corpo';
+import { FundoMobile } from './components/FundoMobile';
+import { NavBar } from './components/NavBar';
+
+function App() {
+  return (
+    <>
+
+      <NavBar />
+
+      <Corpo />
+
+      <FundoMobile />
+    </>
+  )
+}
+
+export default App;
