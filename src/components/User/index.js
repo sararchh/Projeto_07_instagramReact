67e1cb39fda90d8eece5eb80318a0583de3a03ca
Sara Rocha de Oliveira <sara_rocha99@hotmail.com>
2022-09-14 22:26:40 -0300

    feat: add handle image

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 
 export function User() {
   const [name, setName] = React.useState(null);
+  const [imageUser, setImageUser] = React.useState(null);
 
   const handleEditName = () => {
     let nameUser = prompt('Digite seu nome:');
     setName(nameUser);
   };
 
+  const handleEditImage = () => {
+    let imageUser = prompt('Digite url da imagem:');
+    setImageUser(imageUser);
+  };
+
   return (
     <div className="usuario">
-      <img src="assets/img/catanacomics.svg" alt='imagem do usuario' />
+      <img onClick={handleEditImage} src={!imageUser ? 'assets/img/catanacomics.svg' : imageUser} alt='imagem do usuario' />
       <div className="texto">
         <strong>catanacomics</strong>
         <span>
